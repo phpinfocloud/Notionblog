48e9477e97bf6a128fd73a9977068c54f036945f
phpinfocloud <pendy9527@gmail.com>
2025-11-11 01:06:06 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,19 +1,1 @@
 import { siteConfig } from '@/lib/config'
-
-/**
- * 驱动版权
- * @returns
- */
-export default function PoweredBy(props) {
-  return (
-    <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered by</span>
-      <a
-        href='https://www.52stcs.com/'
-        className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
-      </a>
-      .
-    </div>
-  )
-}
